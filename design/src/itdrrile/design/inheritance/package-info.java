eb92b9f3baebf158bd79e66349de034f071a79b3
itdrrile <itdrrile@hotmail.com>
2018-07-13 16:40:43 +0800

    base -> design 003

--- design/src/itdrrile/design/inheritance/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * java继承
- * @author itdrrile
- *
- */
-package itdrrile.design.inheritance;
--- design/test/itdrrile/test/design/inheritance/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author itdrrile
- *
- */
-package itdrrile.test.design.inheritance;
